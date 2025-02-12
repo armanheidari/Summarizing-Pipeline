a8ef537c1398dd226038059d1f326f7b830644b6
Arman Heidari <armanheidari192@gmail.com>
2025-02-13 00:57:09 +0330

    Add main script for testing Vosk speech-to-text transcription

---
+++ src/transcription/test/main.py
@@ -0,0 +1,14 @@
+import sys
+
+from path_handler import PathManager
+
+path_manager = PathManager()
+sys.path.append(str(path_manager.get_base_directory()))
+
+from src.transcription.base import Language, Provider
+from src.transcription.factory import SpeechToTextFactory
+
+if __name__ == "__main__":
+    stt = SpeechToTextFactory.create(Provider.VOSK, Language.ENGLISH)
+    
+    stt.transcribe(str(path_manager.get_base_directory() / "audios/english.wav"))
